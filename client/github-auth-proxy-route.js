bffa90d056d4264ebbaa582ac4d7dc2cdb9d8016
Charanjit Singh <ckhabra007@gmail.com>
2015-04-22 13:36:47 +0530

    Initial commit

---
+++ client/github-auth-proxy-route.js
@@ -0,0 +1,34 @@
+Router.route('/',{
+  template: 'nucleus'
+});
+
+Router.route('/login-with-github', {
+  template: 'nucleus',
+  action: function() {
+    var subdomain = this.params.query.subdomain,
+        serverLoginUrl = subdomain + '/nucleus-github-login'
+    ;
+    if (!subdomain)
+      throw new Meteor.Error('"subdomain" query parameter required');
+
+    $.cookie('nucleus-github-auth-domain', subdomain);
+    window.location.href = serverLoginUrl;
+  }
+});
+
+Router.route('/github-auth', {
+  name: 'github-auth',
+  template: 'nucleus',
+  action: function() {
+    if (this.params.query.subdomain) {
+      this.redirect('/login-with-github?subdomain='+this.params.query.subdomain);
+    }
+
+    var queryString = window.location.href.split('?')[1],
+        subdomain = $.cookie('nucleus-github-auth-domain'),
+        callbackUri = subdomain + '/nucleus-github-callback?' + queryString ;
+
+    $.cookie('nucleus-github-auth-domain', null); //let's reset this cookie
+    window.location.href = callbackUri;
+  }
+});
